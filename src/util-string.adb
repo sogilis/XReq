d97c0c824a085d9251f1309029921bfdbce6304b
Shanti Bouchez-Mongardé <shanti.bouchez@gmail.com>
2010-02-05 11:57:35 +0100

    Fix coverage tests by removing some empty packages

--- src/util-string.adb
+++
@@ -1,5 +0,0 @@
---                         Copyright (C) 2010, Sogilis                       --
-
-package body Util.String is
-
-end Util.String;
--- src/util-string.ads
+++
@@ -1,5 +0,0 @@
---                         Copyright (C) 2010, Sogilis                       --
-
-package Util.String is
-
-end Util.String;
