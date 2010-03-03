0d226ed9055ebd18410e111a6ac808a7ee98dd63
Shanti Bouchez-Mongardé <shanti.bouchez@gmail.com>
2010-03-03 15:14:01 +0100

    Update adaspec CLI

--- src/adaspec-cli.adb
+++ src/adaspec-cli.adb
@@ -7,6 +7,11 @@ package body AdaSpec.CLI is
    procedure Help is
    begin
 
+      pragma Style_Checks (Off);
+
+      --         0         10        20        30        40        50        60        70
+      --         ---------------------------------------------------------------------------XXX
+
       Put_Line ("adaspec - generate tests from specification");
       Put_Line ("");
       Put_Line ("SYNOPSIS");
@@ -15,58 +20,55 @@ package body AdaSpec.CLI is
       Put_Line ("");
       Put_Line ("DESCRIPTION");
       Put_Line ("");
-      Put_Line ("    AdaSpec  takes   as  input  feature   files  describing");
-      Put_Line ("    specifications and generate tests for these features in");
-      Put_Line ("    languages such as Ada, C, C++ depending on the template");
-      Put_Line ("    used.");
+      Put_Line ("    AdaSpec takes as input feature files describing specifications and");
+      Put_Line ("    generate tests for these features in languages such as Ada, C, C++.");
       Put_Line ("");
-      Put_Line ("    Specifications  are described  in a  formalized natural");
-      Put_Line ("    language (see cucumber documentation) and the sentences");
-      Put_Line ("    are translated in code using  steps. These steps define");
-      Put_Line ("    a relation between  the regular expression matching the");
-      Put_Line ("    sentance  and  a function to perform what  the sentance");
-      Put_Line ("    describes.");
+      Put_Line ("    Specifications are described in a formalized natural language (see");
+      Put_Line ("    cucumber documentation) and the sentences are translated in code using");
+      Put_Line ("    using steps. These steps define a relation between the regular");
+      Put_Line ("    expression matching the sentance and a function to perform what the");
+      Put_Line ("    sentance describes.");
       Put_Line ("");
       Put_Line ("OPTIONS");
       Put_Line ("");
       Put_Line ("    FEATURE");
-      Put_Line ("        The   .feature   file   containing    cucumber-like");
-      Put_Line ("        specification.  It is possible  to specify multiple");
-      Put_Line ("        features on  the command-line  all of them will  be");
-      Put_Line ("        processed");
+      Put_Line ("        The .feature file containing cucumber-like specification. It is");
+      Put_Line ("        possible to specify multiple features on the command-line all of");
+      Put_Line ("        them will be processed.");
       Put_Line ("");
       Put_Line ("    -h, -help, --help");
       Put_Line ("        Help message");
       Put_Line ("");
       Put_Line ("    -s, --step STEPDIR");
-      Put_Line ("        Specify  a step  directory.  By default,  the step/");
-      Put_Line ("        directory relative  to each feature  file specified");
-      Put_Line ("        is searched.");
+      Put_Line ("        Specify a step directory.  By default, the step/ directory relative");
+      Put_Line ("        to each feature file specified is searched.");
       Put_Line ("");
-      Put_Line ("    -k, --keep-going             (NOT IMPLEMENTED)");
-      Put_Line ("        Keep going if  there are  errors for a  feature and");
-      Put_Line ("        compile the next feature.");
+      Put_Line ("    -k, --keep-going");
+      Put_Line ("        Keep going if there are errors for a feature and compile the next");
+      Put_Line ("        feature. ");
       Put_Line ("");
-      Put_Line ("    -x, --executable TEST_NAME   (NOT IMPLEMENTED)");
-      Put_Line ("        Create a test program  that will ne able to run all");
-      Put_Line ("        the  specified  features  called TEST_NAME.  If not");
-      Put_Line ("        present, only test packages are created.");
-      Put_Line ("        The TEST_NAME  is relative  to the output directory");
+      Put_Line ("    -x, --executable TEST_NAME");
+      Put_Line ("        Create a test program called TEST_NAME that will be able to run all");
+      Put_Line ("        the specified features.  If not present, only test packages are");
+      Put_Line ("        created.  The TEST_NAME is relative to the output directory");
       Put_Line ("        specified with the -o (--output-dir) option.");
       Put_Line ("");
       Put_Line ("    -o, --output DIR");
-      Put_Line ("        Choose  in  which directory  to generate  the files");
-      Put_Line ("        generated.  They have  the same name as the feature");
-      Put_Line ("        files.");
-      Put_Line ("        By  default,  they  are  generated  in  the  tests/");
-      Put_Line ("        directory relative to the feature file.");
+      Put_Line ("        Choose in which directory the files will be generated. The test");
+      Put_Line ("        packages have the same name as the features.  By default the output");
+      Put_Line ("        directory is the tests/ directory relative to the feature file.");
       Put_Line ("");
       Put_Line ("    -l, --lang LANG");
       Put_Line ("        Choose wich language to generate.");
-      Put_Line ("        Available values: Ada");
+      Put_Line ("        Available languages: Ada");
       Put_Line ("");
       Put_Line ("-- ");
       Put_Line ("Copyright (c) 2010 SOGILIS");
+
+      --         ---------------------------------------------------------------------------XXX
+      --         0         10        20        30        40        50        60        70
+
+      pragma Style_Checks (On);
 
    end Help;
 
