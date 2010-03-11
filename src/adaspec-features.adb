6e084bb11893347c776802e4f3a6e31c2259f313
Shanti Bouchez-Mongardé <shanti.bouchez@gmail.com>
2010-03-11 11:19:16 +0100

    TRIED: Finite State Machine to parse features, difficult, reverting

--- src/adaspec-features.adb
+++ src/adaspec-features.adb
@@ -128,71 +128,218 @@ package body AdaSpec.Features is
 
       type Mode_Type is (M_Begin, M_Feature, M_Background, M_Scenario, M_Str);
 
-      K_Feature     : constant String := "Feature:";
-      K_Background  : constant String := "Background:";
-      K_Scenario    : constant String := "Scenario:";
-      K_Given       : constant String := "Given ";
-      K_When        : constant String := "When ";
-      K_Then        : constant String := "Then ";
-      K_And         : constant String := "And ";
-      K_StrDouble   : constant String := """""""";
-      K_StrSimple   : constant String := "'''";
-      CurrentStr    : String (1 .. 3);
-      State         : Mode_Type         := M_Begin;
-      State_Saved   : Mode_Type;
-      Stanza_State  : Prefix_Type_Maybe := Prefix_None;
-      Has_Stanza    : Boolean := False;
-      File          : File_Type;
-      Line_S        : Unbounded_String;
-      Suffix        : Unbounded_String;
-      Long_String   : Unbounded_String;
-      Idx_Start     : Natural;
-      Idx_Start_Str : Natural;
-      Position      : Position_Type;
+      --  The keywords
+      K_Feature        : constant String := "Feature:";
+      K_Background     : constant String := "Background:";
+      K_Scenario       : constant String := "Scenario:";
+      K_Given          : constant String := "Given ";
+      K_When           : constant String := "When ";
+      K_Then           : constant String := "Then ";
+      K_And            : constant String := "And ";
+      K_StrDouble      : constant String := """""""";
+      K_StrSimple      : constant String := "'''";
 
-      Current_Scenario : Scenario_Type;
+      --  State machine
+      Curr_State       : Mode_Type         := M_Begin;     --  Current State
+      State_Saved      : Mode_Type;                        --  Previous State
+      Stanza_State     : Prefix_Type_Maybe := Prefix_None; --  Kind of step
+
+      --  General purpose variables
+      File             : File_Type;         --  Current file
+      Position         : Position_Type;     --  Position in file
+      Line_S           : Unbounded_String;  --  Current line
+      Idx_Start        : Natural;           --  First non blank character index
+
+
+      --  Current stanza
       Current_Stanza   : Stanza_Type;
+      Has_Stanza       : Boolean := False;
 
-      function Starts_With_K (Keyword : in String) return Boolean;
-      function Starts_With_K (Keyword : in String) return Boolean is
+      --  Current multi-line argument
+      CurrentStr       : String (1 .. 3);       --  Long string type """ or '''
+
+      Suffix           : Unbounded_String;
+      Long_String      : Unbounded_String;
+      Idx_Start_Str    : Natural;
+
+      -------------------------------------------------
+      --  GENERAL INFORMATION ON THE STATE MACHINES  --
+      -------------------------------------------------
+      --  Parsing is line oriented, on each line, a transition on the current
+      --  state machine is triggered.
+      --
+      --  The state machine gather information about the current block geing
+      --  parsed.
+
+      -----------------------------------------
+      --  FINITE STATE MACHINE  --  LEVEL 0  --
+      -----------------------------------------
+
+      type Command_Type is (Cmd_Reset, Cmd_Start, Cmd_Run);
+
+      type State_0_Type is (S0_Init, S0_Feature, S0_Background, S0_Scenario);
+      type State_1_Type renames Prefix_Type_Maybe;
+      type State_2_Type is (S2_Init, S2_String, S2_Table);
+
+      State_0 : State_0_Type := S0_Init;
+      State_1 : State_1_Type := Prefix_None;
+      State_2 : State_2_Type := S2_Init;
+
+      procedure FSM_Level_0 is
+         New_Scenario : Boolean := False;
       begin
-         return Starts_With (To_String (Line_S), Keyword, Idx_Start);
-      end Starts_With_K;
+         case State_0 is
+            when S0_Init =>
 
+               --  Found: "Feature: Name"
+               if Starts_With_K (K_Feature) then
+                  State_0   := S0_Feature;
+                  Self.Name := Trimed_Suffix (Line_S,
+                                              Idx_Start + K_Feature'Length);
+               end if;
+
+            when S0_Feature | S0_Background | S0_Scenario =>
+
+               --  Found "Background:"
+               if Starts_With_K (K_Background) then
+                  New_Scenario := True;
+                  State_O      := S0_Background;
+                  Idx_Start    := Idx_Start + K_Background'Length;
+
+               --  Found "Scenario: Name"
+               elsif Starts_With_K (K_Scenario) then
+                  New_Scenario := True;
+                  State_O      := S0_Scenario;
+                  Idx_Start    := Idx_Start + K_Scenario'Length;
+               end if;
+
+               if New_Scenario then
+                  
+
+               --  In a scenario, go to level 1
+               elsif State_0 /= S0_Feature then
+                  FSM_Level_1;
+               end if;
+
+         end case;
+      end FSM_Level_0;
+
+      procedure FSM_Level_1 is
+      begin
+         case State_1 is
+            when Prefix_None =>
+
+            when Prefix_Given | Prefix_When | Prefix_Then =>
+
+               --  Found "Background:"
+               if Starts_With_K (K_Background) then
+                  New_Scenario (K_Background, M_Background);
+
+               --  Found "Scenario: Name"
+               elsif Starts_With_K (K_Scenario) then
+                  New_Scenario (K_Scenario, M_Scenario);
+
+               --  In a scenario, go to level 1
+               elsif State_0 /= S0_Feature then
+                  FSM_Level_1;
+               end if;
+
+         end case;
+      end FSM_Level_1;
+
+      ------------------------------------------------
+      --  GENERAL INFORMATION ON THE STATE MACHINE  --
+      ------------------------------------------------
+      --  For each state, there are generally two procedures:
+      --  New_<Element>:
+      --      This procedure is called when entering the state. It initialize
+      --      the state variables and the current element.
+      --  Add_<Element>:
+      --      This procedure is called when another element of the same
+      --      hierarchy level starts. It takes the current element variables 
+      --      and save them in the current element object.
+      --      Then it reset the variables it used for a new element
+      --      This is generally called in a New_<Element> procedure
+      --
+      --  There are sub state machines for each scenarios. When in a scenario,
+      --  the `Stanza_State' variable holds the state of the current scenario.
+      --
+      --  When parsing a multi line argument (long string for example) the 
+      --  state machine exit the current 
+
+      procedure New_Feature;
+      procedure New_Scenario (Keyword : in String; Typ : in Mode_Type);
       procedure Add_Scenario;
       procedure Add_Stanza;
       procedure Add_Text;
 
+      ------------
+      --  INIT  --
+      ------------
+      --  This is the initialization state.
+      --  Going to FEATURE when we find the Feature keyword
+      --  Scenarios are not allowed at top level
+
+      ---------------
+      --  FEATURE  --
+      ---------------
+      --  This is when the feature starts, when no scenarious have been found
+      --  In this, the lines of text are added to the feature description
+      --  When a scenario is found, go to the SCENARIO state
+
+      procedure New_Feature is
+      begin
+         State_0      := S0_Feature;
+         Curr_State   := M_Feature;
+         Stanza_State := Prefix_None;
+         Self.Name    := Trimed_Suffix (Line_S, Idx_Start + K_Feature'Length);
+      end New_Feature;
+
+      -----------------------------
+      --  BACKGROUND / SCENARIO  --
+      -----------------------------
+      --  This state is entereg when a scenario keyword is found and we are in
+      --  the SCENARIO or FEATURE state.
+      --  This state is not left until
+
+      Current_Scenario : Scenario_Type;
+
+      procedure New_Scenario (Keyword : in String; Typ : in Mode_Type) is
+      begin
+         Add_Scenario;
+         Curr_State := Typ;
+         Stanza_State := Prefix_None;
+         Current_Scenario.Name :=
+            Trimed_Suffix (Line_S, Idx_Start + Keyword'Length);
+      end New_Scenario;
+
       procedure Add_Scenario is
       begin
          Add_Stanza;
-         if State = M_Scenario then
+         if Curr_State = M_Scenario then
             Append (Self.Scenarios, Current_Scenario);
---             Put_Line ("Found Scenario: " &
---                       To_String (Current_Scenario.Name));
-         elsif State = M_Background then
+         elsif Curr_State = M_Background then
             Self.Background.Stanzas := Current_Scenario.Stanzas;
---             Put_Line ("Found Background: " &
---                       To_String (Current_Scenario.Name));
          end if;
          Current_Scenario.Name := Null_Unbounded_String;
          Clear (Current_Scenario.Stanzas);
       end Add_Scenario;
 
+      --  Called when a step or a scanario starts. This saves the previous step
+      --  in the current scenario.
       procedure Add_Stanza is
       begin
          if Has_Stanza then
---             Put_Line ("Found Stanza: " & To_String (Current_Stanza));
             Append (Current_Scenario.Stanzas, Current_Stanza);
             Has_Stanza     := False;
          end if;
          Current_Stanza := Null_Stanza;
       end Add_Stanza;
 
+      --  Called when a new multi-line element, a step or a scenario starts. 
+      --  This saves the current multi-line argument in the current step
       procedure Add_Text is
       begin
---          Put_Line ("Found String: " & CurrentStr &
---                    To_String (Long_String) & CurrentStr);
          Append (Current_Stanza.Texts, Long_String);
          Long_String := Null_Unbounded_String;
       exception
@@ -203,6 +350,14 @@ package body AdaSpec.Features is
             Long_String := Null_Unbounded_String;
          --  GCOV_IGNORE_END
       end Add_Text;
+
+
+      --  Return true if the current line starts with the keyword `Keyword'
+      function Starts_With_K (Keyword : in String) return Boolean;
+      function Starts_With_K (Keyword : in String) return Boolean is
+      begin
+         return Starts_With (To_String (Line_S), Keyword, Idx_Start);
+      end Starts_With_K;
 
    begin
       Errors := False;
@@ -217,16 +372,15 @@ package body AdaSpec.Features is
          Line_S    := Get_Whole_Line (File);
          Idx_Start := Index_Non_Blank (Line_S);
 
---          Put_Line ("READ: " & To_String (Line_S));
          --
          --  State Machine
          --
-         case State is
+         case Curr_State is
             when M_Begin =>
 
                --  Found: "Feature: Name"
                if Starts_With_K (K_Feature) then
-                  State := M_Feature;
+                  Curr_State := M_Feature;
                   Stanza_State := Prefix_None;
                   Self.Name := Trimed_Suffix (Line_S,
                                            Idx_Start + K_Feature'Length);
@@ -236,38 +390,22 @@ package body AdaSpec.Features is
 
                --  Found "Background:"
                if Starts_With_K (K_Background) then
-                  Add_Scenario;
-                  State := M_Background;
-                  Stanza_State := Prefix_None;
-                  Self.Background.Name :=
-                     Trimed_Suffix (Line_S, Idx_Start + K_Background'Length);
+                  New_Scenario (K_Background, M_Background);
 
                --  Found "Scenario: Name"
                elsif Starts_With_K (K_Scenario) then
-                  Add_Scenario;
-                  State := M_Scenario;
-                  Stanza_State := Prefix_None;
-                  Current_Scenario.Name :=
-                     Trimed_Suffix (Line_S, Idx_Start + K_Scenario'Length);
+                  New_Scenario (K_Scenario, M_Scenario);
                end if;
 
             when M_Background | M_Scenario =>
 
                --  Found "Background:"
                if Starts_With_K (K_Background) then
-                  Add_Scenario;
-                  State := M_Background;
-                  Stanza_State := Prefix_None;
-                  Self.Background.Name :=
-                     Trimed_Suffix (Line_S, Idx_Start + K_Background'Length);
+                  New_Scenario (K_Background, M_Background);
 
                --  Found "Scenario: Name"
                elsif Starts_With_K (K_Scenario) then
-                  Add_Scenario;
-                  State := M_Scenario;
-                  Stanza_State := Prefix_None;
-                  Current_Scenario.Name :=
-                     Trimed_Suffix (Line_S, Idx_Start + K_Scenario'Length);
+                  New_Scenario (K_Scenario, M_Scenario);
 
                --  Found "Given ..."
                elsif Starts_With_K (K_Given) then
@@ -302,28 +440,28 @@ package body AdaSpec.Features is
 
                --  Found """
                elsif Starts_With_K (K_StrDouble) then
-                  State_Saved  := State;
-                  State         := M_Str;
+                  State_Saved  := Curr_State;
+                  Curr_State         := M_Str;
                   CurrentStr    := K_StrDouble;
                   Idx_Start_Str := Idx_Start;
                   Idx_Start     := Idx_Start + CurrentStr'Length;
 
                --  Found '''
                elsif Starts_With_K (K_StrSimple) then
-                  State_Saved   := State;
-                  State         := M_Str;
+                  State_Saved   := Curr_State;
+                  Curr_State         := M_Str;
                   CurrentStr    := K_StrSimple;
                   Idx_Start_Str := Idx_Start;
                   Idx_Start     := Idx_Start + CurrentStr'Length;
                end if;
 
                --  Record first line of long string
-               if State = M_Str then
+               if Curr_State = M_Str then
                   if Index_Non_Blank (Line_S, Idx_Start) /= 0 then
                      Put_Line ("Error: PyString: found text after " &
                                CurrentStr);
                      Errors := True;
-                     State  := State_Saved;
+                     Curr_State  := State_Saved;
                   end if;
 
                --  Continue the stanza on the next line
@@ -351,14 +489,14 @@ package body AdaSpec.Features is
                   if Starts_With_K (CurrentStr) then
                      Idx_Start := Idx_Start + CurrentStr'Length;
                      if Index_Non_Blank (Line, Idx_Start) = 0 then
-                        State := State_Saved;
+                        Curr_State := State_Saved;
                         --  End of string, remove the last line feed.
                         Head (Long_String, Length (Long_String) - 1);
                         Add_Text;
                      end if;
                   end if;
 
-                  if State = M_Str then
+                  if Curr_State = M_Str then
                      I := Natural'Min (Index_Non_Blank (Line), Idx_Start_Str);
                      I := Natural'Max (I, 1);
                      while I <= Line'Last loop
@@ -377,7 +515,6 @@ package body AdaSpec.Features is
                end;
 
          end case;
---          Put_Line ("STATE: " & State'Img & " " & Stanza_State'Img);
 
          Position.Line := Position.Line + 1;
       end loop;
