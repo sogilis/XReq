902f8a582fc0762e25ce85666848045af703ed21
Shanti Bouchez-Mongardé <shanti.bouchez@gmail.com>
2010-03-17 18:17:32 +0100

    AdaSpecLib.String_Tables now accept String arguments

---
+++ src/lib/adaspeclib-string_tables.adb
@@ -0,0 +1,24 @@
+--                         Copyright (C) 2010, Sogilis                       --
+
+package body AdaSpecLib.String_Tables is
+
+   function  Item    (T    : in Table;
+                      X, Y : in Integer) return String is
+   begin
+      return To_String (Item (T, X, Y));
+   end Item;
+
+   procedure Put     (T    : in out Table;
+                      X, Y : in     Integer;
+                      Elem : in     String) is
+   begin
+      Put (T, X, Y, To_Unbounded_String (Elem));
+   end Put;
+
+   function  Element     (C : in     Cursor) return String is
+   begin
+      return To_String (Element (C));
+   end Element;
+
+
+end AdaSpecLib.String_Tables;
--- src/lib/adaspeclib-string_tables.ads
+++ src/lib/adaspeclib-string_tables.ads
@@ -5,5 +5,38 @@ with AdaSpecLib.Tables;
 
 use Ada.Strings.Unbounded;
 
-package AdaSpecLib.String_Tables is new AdaSpecLib.Tables  --  GCOV_IGNORE
-   (Unbounded_String, "=");
+package AdaSpecLib.String_Tables is
+
+   package Unbounded_String_Tables is new AdaSpecLib.Tables
+      (Unbounded_String, "=");
+
+   subtype Element_Type is Unbounded_String;
+   subtype Key_Type     is Unbounded_String_Tables.Key_Type;
+
+   function "<" (Left, Right : in Key_Type) return Boolean
+      renames Unbounded_String_Tables."<";
+
+   type    Table  is new Unbounded_String_Tables.Table with null record;
+   subtype Cursor is     Unbounded_String_Tables.Cursor;
+
+   function  Item    (T    : in Table;
+                      X, Y : in Integer) return String;
+   procedure Put     (T    : in out Table;
+                      X, Y : in     Integer;
+                      Elem : in     String);
+   function  Element (C    : in     Cursor) return String;
+
+
+
+
+   procedure Next        (C : in out Cursor)
+      renames Unbounded_String_Tables.Next;
+   function  Element     (C : in     Cursor) return Element_Type
+      renames Unbounded_String_Tables.Element;
+   function  Key         (C : in     Cursor) return Key_Type
+      renames Unbounded_String_Tables.Key;
+   function  Has_Element (C : in     Cursor) return Boolean
+      renames Unbounded_String_Tables.Has_Element;
+
+end AdaSpecLib.String_Tables;
+
