f5e0117c72bcfd71e0c35ab664e152f337601475
Shanti Bouchez-Mongardé <shanti.bouchez@gmail.com>
2010-03-29 17:42:10 +0200

    Revert "another dead end"
    This reverts commit 86b55fb08d97ae76257f1f3365ffe409afe375a1.

---
+++ src/lib/adaspeclib.ads
@@ -0,0 +1,15 @@
+--                         Copyright (C) 2010, Sogilis                       --
+
+package AdaSpecLib is
+
+   Not_Yet_Implemented : exception;
+
+   -----------------
+   --  Step_Type  --
+   -----------------
+
+   type Step_Type is                            --  GCOV_IGNORE
+      (Step_Given, Step_When, Step_Then);       --  GCOV_IGNORE
+
+
+end AdaSpecLib;
