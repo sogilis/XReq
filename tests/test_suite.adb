4915f5d432bef37d866e6609cfba0866e5e567a6
Shanti Bouchez-Mongardé <shanti@sogilis.com>
2010-08-11 10:10:54 +0200

    ... WIP memory corruption bug ...

--- tests/test_suite.adb
+++ tests/test_suite.adb
@@ -20,7 +20,6 @@
 with Ada.Text_IO;
 
 with Test_Suite.IO;
-with Test_Suite.Smart;
 with Test_Suite.Strings;
 with Test_Suite.Strings.Pool;
 
@@ -58,7 +57,6 @@ package body Test_Suite is
    begin
 
       Test_Suite.IO             .Add_Tests (Ret);
-      Test_Suite.Smart          .Add_Tests (Ret);
       Test_Suite.Strings        .Add_Tests (Ret);
       Test_Suite.Strings.Pool   .Add_Tests (Ret);
 
